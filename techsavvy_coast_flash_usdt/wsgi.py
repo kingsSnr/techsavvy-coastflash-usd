ca9bfda46924266b142e699b757759ec3df6acf4
aisavvytoday <muhuri.json@gmail.com>
2024-08-04 13:11:50 +0300

    all set up, let do this Kings

---
+++ techsavvy_coast_flash_usdt/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for techsavvy_coast_flash_usdt project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/4.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'techsavvy_coast_flash_usdt.settings')
+
+application = get_wsgi_application()
